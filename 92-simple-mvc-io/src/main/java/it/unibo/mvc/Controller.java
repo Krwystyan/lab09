d035138d52944d052d13c0019d70ccd0dcd8ca87
Cristian Di Donato - cristian.didonato@studio.unibo.it <cristian.didonato@studio.unibo.it>
2024-11-18 12:50:57 +0100

    The 92 have to implement 'SimpleGUIWithFileChooser' and test with build

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,41 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import javax.swing.JOptionPane;
 
 /**
  * Application controller. Performs the I/O.
  */
-public class Controller {
+
+public class Controller implements java.io.Serializable{
+    private static final long serialVersionUID = 1;
+    private static final String DEFAULTPATH = System.getProperty("user.home")
+        + System.getProperty("file.separator")
+        + "output.txt";
+    private File consideredFile = new File(DEFAULTPATH);
+
+    public File getFile() {
+        return this.consideredFile;
+    }
+    public void setFile(final File passedFile) {
+        this.consideredFile =  passedFile;
+    }
+    public String getFilePath() {
+        return this.consideredFile.getPath();
+    }
+    public void writeAString(String inputPassed) throws FileNotFoundException {
+        try(PrintStream p = new PrintStream(consideredFile, StandardCharsets.UTF_8)) {
+            p.println(inputPassed);
+        } catch(IOException ioException) {
+            System.out.println("An error during write the file: " + ioException.getMessage());
+            ioException.getStackTrace();
+        }
+    }
+
 
 }
--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,18 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+import java.awt.Desktop.Action;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +20,61 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
+    private static final int PROPORTION = 5;
     private final JFrame frame = new JFrame();
+    private final Controller controller = new Controller();
+
+    public SimpleGUI(){
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        final JTextField textField = new JTextField();
+        final JButton savButton = new JButton("Save");
+        canvas.add(textField, BorderLayout.CENTER);
+        canvas.add(savButton, BorderLayout.SOUTH);
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        savButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                try{
+                    controller.writeAString(textField.getText());
+                } catch (NullPointerException | FileNotFoundException nullPointerException) {
+                    JOptionPane.showMessageDialog(frame, nullPointerException, "Error", JOptionPane.ERROR_MESSAGE);
+                    nullPointerException.printStackTrace();
+                }
+                
+            }
+        });
+    }
+
+    private void display() {
+        /*
+         * Make the frame one fifth the resolution of the screen. This very method is
+         * enough for a single screen setup. In case of multiple monitors, the
+         * primary is selected. In order to deal coherently with multimonitor
+         * setups, other facilities exist (see the Java documentation about this
+         * issue). It is MUCH better than manually specify the size of a window
+         * in pixel: it takes into account the current resolution.
+         */
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        /*
+         * Instead of appearing at (0,0), upper left corner of the screen, this
+         * flag makes the OS window manager take care of the default positioning
+         * on screen. Results may vary, but it is generally the best choice.
+         */
+        frame.setLocationByPlatform(true);
+        /*
+         * OK, ready to push the frame onscreen
+         */
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args){
+        new SimpleGUI().display();
+    }
 
 }
