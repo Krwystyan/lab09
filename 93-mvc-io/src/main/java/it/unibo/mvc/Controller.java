39cbc64af867338d17fd390c6eb2a2e1bc2c8639
Cristian Di Donato - cristian.didonato@studio.unibo.it <cristian.didonato@studio.unibo.it>
2024-11-20 17:25:24 +0100

    Now 93 have the interface controller, in the last commit, for some reason is not present

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,13 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
-
+    void setNextString(final String x);
+    String getNextString();
+    List<String> getHistory();
+    void printCurrentString();
 }
